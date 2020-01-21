4e971de67f39245d047757977b3d871d37274272
USAMI Kenta <tadsan@zonu.me>
2020-01-21 13:44:37 +0900

    Add config.php

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'parts' => [
+        'header' => __DIR__ . '/sample/_header.php',
+        //'footer' => __DIR__ . '/sample/_footer.php',
+    ],
+    'content' => __DIR__ . '/sample',
+];
